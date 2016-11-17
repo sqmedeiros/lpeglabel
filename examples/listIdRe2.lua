96284f8b4a6a25efd3c0c5ce9c7595604ba3143f
Sergio Queiroz <sqmedeiros@gmail.com>
2016-11-17 15:55:24 -0300

    Updating examples using the recovery operator

--- examples/listIdRe2.lua
+++ examples/listIdRe2.lua
@@ -1,4 +1,4 @@
-local re = require 'relabel' 
+local re = require 'relabelrec' 
 
 local errinfo = {
   {"errUndef",  "undefined"},
@@ -18,23 +18,45 @@ re.setlabels(labels)
 
 local g = re.compile[[
   S      <- Id List
-  List   <- !.  /  (',' /  %{errComma}) (Id / %{errId}) List
-  Id     <- Sp [a-z]+
-  Comma  <- Sp ','
+  List   <- !.  /  Comma Id List
+  Id     <- Sp [a-z]+ / %{errId}
+  Comma  <- Sp ',' / %{errComma}
   Sp     <- %s*
 ]]
 
+local errors
+
+function recorderror (subject, pos, label)
+	local line, col = re.calcline(subject, pos)
+	table.insert(errors, { line = line, col = col, msg = errmsgs[labels[label]] })
+	return true 
+end
+
+function sync (p)
+	return '( !(' .. p .. ') .)*'
+end
+
+local grec = re.compile(
+  "S         <- %g //{errComma} ErrComma //{errId} ErrId" .. "\n" ..
+  "ErrComma  <-  ('' -> 'errComma' => recorderror) " .. sync('!. / [a-z]+') .. "\n" ..
+	"ErrId     <-  ('' -> 'errId' => recorderror) (!(!. / ',') .)*"
+	, {g = g, recorderror  = recorderror})
+
 function mymatch (g, s)
-  local r, e, sfail = g:match(s)
-  if not r then
-    local line, col = re.calcline(s, #s - #sfail)
-    local msg = "Error at line " .. line .. " (col " .. col .. "): "
-    return r, msg .. errmsgs[e] .. " before '" .. sfail .. "'"
+	errors = {}
+	local r, e, sfail = g:match(s)
+	if #errors > 0 then
+		local out = {}
+    for i, err in ipairs(errors) do
+    	local msg = "Error at line " .. err.line .. " (col " .. err.col .. "): " .. err.msg
+      table.insert(out,  msg)
+    end
+    return nil, table.concat(out, "\n")
   end
   return r
 end
-
-print(mymatch(g, "one,two"))
-print(mymatch(g, "one two"))
-print(mymatch(g, "one,\n two,\nthree,"))
-
+  
+print(mymatch(grec, "one,two"))
+print(mymatch(grec, "one two three"))
+print(mymatch(grec, "1,\n two, \n3,"))
+print(mymatch(grec, "one\n two123, \nthree,"))
