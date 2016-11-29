a4bdec34353ad1849f99e6ab0e5f2ad5e74c83b1
Sergio Queiroz <sqmedeiros@gmail.com>
2016-11-29 18:38:55 -0300

    New example shows how to build recovery grammar semiautomatically

--- examples/expressionRecAut.lua
+++ examples/expressionRecAut.lua
@@ -5,31 +5,30 @@ local R, S, P, V = m.R, m.S, m.P, m.V
 local C, Cc, Ct, Cmt = m.C, m.Cc, m.Ct, m.Cmt
 local T, Rec = m.T, m.Rec
 
-local labels = {
-  {"ExpTermFirst",  "expected an expression"},
-  {"ExpTermOp",   "expected a term after the operator"},
-  {"MisClose",  "missing a closing ')' after the expression"},
-}
+local num = R("09")^1 / tonumber
+local op = S("+-")
 
-local function labelindex(labname)
-  for i, elem in ipairs(labels) do
-    if elem[1] == labname then
-      return i
-    end
-  end
-  error("could not find label: " .. labname)
+local labels = {}
+local nlabels = 0
+
+local function newError(lab, msg, psync, pcap)
+	nlabels = nlabels + 1
+	psync = psync or m.P(-1)
+	pcap = pcap or m.P""
+	labels[lab] = { id = nlabels, msg = msg, psync = psync, pcap = pcap }
 end
+
+newError("ExpTermFirst", "expected an expression", op + ")", m.Cc(1000)) 
+newError("ExpTermOp", "expected a term after the operator", op + ")", m.Cc(1000))
+newError("MisClose",  "missing a closing ')' after the expression",  m.P")")
+newError("Extra", "extra characters found after the expression") 
 
 local errors, subject
 
-local function expect(patt, labname, recpatt)
-  local i = labelindex(labname)
+local function expect(patt, labname)
+  local i = labels[labname].id
   return patt + T(i)
 end
-
-
-local num = R("09")^1 / tonumber
-local op = S("+-")
 
 local function compute(tokens)
   local result = tokens[1]
@@ -56,11 +55,11 @@ local g = P {
 
 function recorderror(pos, lab)
 	local line, col = re.calcline(subject, pos)
-	table.insert(errors, { line = line, col = col, msg = labels[lab][2] })
+	table.insert(errors, { line = line, col = col, msg = labels[lab].msg })
 end
 
 function record (labname)
-	return (m.Cp() * m.Cc(labelindex(labname))) / recorderror
+	return (m.Cp() * m.Cc(labname)) / recorderror
 end
 
 function sync (p)
@@ -71,11 +70,16 @@ function defaultValue (p)
 	return p or m.Cc(1000) 
 end
 
+local recg2 = g
+for k, v in pairs(labels) do
+	recg2 = Rec(recg2, record(k) * sync(v.psync) * v.pcap, v.id)
+end
+
 local recg = P {
 	"S",
-	S = Rec(V"A", V"ErrExpTermFirst", labelindex("ExpTermFirst")), -- default value is 0
-	A = Rec(V"Sg", V"ErrExpTermOp", labelindex("ExpTermOp")),
-	Sg = Rec(g, V"ErrMisClose", labelindex("MisClose")),
+	S = Rec(V"A", V"ErrExpTermFirst", labels["ExpTermFirst"].id), -- default value is 0
+	A = Rec(V"Sg", V"ErrExpTermOp", labels["ExpTermOp"].id),
+	Sg = Rec(g, V"ErrMisClose", labels["MisClose"].id),
 	ErrExpTermFirst = record("ExpTermFirst") * sync(op + ")") * defaultValue(),
 	ErrExpTermOp = record("ExpTermOp") * sync(op + ")") * defaultValue(),
 	ErrMisClose = record("MisClose") * sync(P")") * defaultValue(m.P""),
@@ -85,7 +89,7 @@ local recg = P {
 local function eval(input)
 	errors = {}
 	subject = input
-  local result, label, suffix = recg:match(input)
+  local result, label, suffix = recg2:match(input)
   if #errors > 0 then
     local out = {}
     for i, err in ipairs(errors) do
@@ -111,7 +115,7 @@ print(eval "1+3+-9")
 --> 1 + 3 + [0] - 9
 
 print(eval "1+()3+")
---> 1 + ([0]) [3 +] [0]
+--> 1 + ([0]) [+] 3 + [0]
 
 print(eval "8-(2+)-5")
 --> 8 - (2 + [0]) - 5 
